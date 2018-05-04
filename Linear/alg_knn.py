f79f82f272cdbb319850727b8f2aada77491a091
42ow0rm <38980815+42ow0rm@users.noreply.github.com>
2018-05-04 15:03:27 +0200

    Update alg_knn.py

--- Linear/alg_knn.py
+++ Linear/alg_knn.py
@@ -46,7 +46,7 @@ classifier.fit(X_train, y_train)
 # Prediction the Test set result
 y_pred = classifier.predict(X_test)
 
-# Making the Confuse Matrix 
+# Making the Confusion Matrix 
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
