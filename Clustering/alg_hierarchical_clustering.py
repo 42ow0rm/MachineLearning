87b3ff695e400292cae696a2ad4669e3102d47da
42ow0rm <38980815+42ow0rm@users.noreply.github.com>
2018-05-04 23:41:58 +0200

    Update alg_hierarchical_clustering.py

--- Clustering/alg_hierarchical_clustering.py
+++ Clustering/alg_hierarchical_clustering.py
@@ -25,11 +25,11 @@ hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = '
 y_hc = hc.fit_predict(X)
 
 # Visualising the clusters
-plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c ='red', label = 'Cluster 1')
-plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c ='green', label = 'Cluster 2')
-plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c ='blue', label = 'Cluster 3')
-plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c ='green', label = 'Cluster 4')
-plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c ='cyan', label = 'Cluster 5')
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c ='red', label = 'Careful')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c ='green', label = 'Standard')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c ='blue', label = 'Target')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c ='green', label = 'Careless')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c ='cyan', label = 'Sensible')
 plt.title('Cluster of clients')
 plt.xlabel('Annual income')
 plt.ylabel('Spening Score (1 - 100)')
